fce02a3ed3743383e3acfd9a46e0359d8eb8beed
Thomas <thomasweaverson@gmail.com>
2026-05-19 18:29:06 +0300

    Раздел 7. Задание 1. Рефакторинг презентера фильтров

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -1,4 +1,5 @@
-import { render } from '../framework/render';
+import { remove, render, replace } from '../framework/render';
+import { UpdateType } from '../utils/const';
 import FilterView from '../view/filter-view';
 
 export default class FilterPresenter {
@@ -6,18 +7,44 @@ export default class FilterPresenter {
   #pointsModel = null;
   #filterModel = null;
 
+  #filterComponent = null;
+
   constructor({ filterContainer, pointsModel, filterModel }) {
     this.#filterContainer = filterContainer;
     this.#pointsModel = pointsModel;
     this.#filterModel = filterModel;
+
+    this.#pointsModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
   }
 
   init() {
-    const currentFilter = this.#filterModel.filter;
-    render(new FilterView({
+    const prevFilterComponent = this.#filterComponent;
+
+    this.#filterComponent = new FilterView({
       points: this.#pointsModel.points,
-      currentFilter,
-      onFilterChange: () => { }
-    }), this.#filterContainer);
+      currentFilter: this.#filterModel.filter,
+      onFilterChange: this.#handleFilterTypeChange
+    });
+
+    if (prevFilterComponent === null) {
+      render(this.#filterComponent, this.#filterContainer);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
   }
+
+  #handleModelEvent = () => {
+    this.init();
+  };
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  };
 }
